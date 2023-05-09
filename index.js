6426b511488eb881a16b0186423be104b081a1e1
DARVCODEV <rojasvidaldaniel@gmail.com>
2023-05-09 09:42:55 -0500

    feat(BACKEND): :sparkles: CRUD de clientes y ajuste de proyectos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { main } = require("./db");
 const proyectoRoutes = require("./lib/proyectos/routes/proyectoRoutes");
+const clienteRoutes = require("./lib/clientes/routes/clienteRoutes");
 
 // Inicializar la app de express
 const app = express();
@@ -13,7 +14,10 @@ app.use(express.json());
 
 // Configurar rutas de la API
 app.use("/proyectos", proyectoRoutes);
+app.use("/clientes", clienteRoutes);
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
+);
